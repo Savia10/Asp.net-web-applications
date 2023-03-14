cb1bafb95f3119d70c8c650c3eff7b0fc2677a8b
Savia Maria Coalco <48923354+Savia10@users.noreply.github.com>
2023-03-14 14:59:19 +0530

    Create Employee.cs

---
+++ ASP.NET MVC - Scaffolding/Models/Employee.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace MVCScaffoldingDemo.Models {
+   public class Employee{
+      public int ID { get; set; }
+      public string Name { get; set; }
+      public DateTime JoiningDate { get; set; }
+      public int Age { get; set; }
+   }
+}
